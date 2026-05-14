d06935d726e63c1c4d6f63a5bb6ff878d8a7e231
TekTS-07 <TekTS-07@TEKWISSEN.HUB>
2026-05-14 02:31:08 -0700

    configured .env properly in git

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.mongodb_URI)
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log(
             "MongoDB connected Successfully"
         )
